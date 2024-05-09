ea197fe3902e68c913aebdce80880f53ccd9e22a
Nithya AJ <aj7nithya@gmail.com>
2024-05-09 14:29:42 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+export const theme = {
+    extend: {
+        colors: {
+            primary: "#7b4ede",
+        },
+        screens: {
+            xs: "320px",
+            sm: "480px",
+            md: "767px",
+            lg: "1025px",
+            xl: "1200px",
+            xxl: "1400px",
+        },
+        fontSizes: {
+            heading: "4.4rem",
+            display1: "3rem",
+            display2: "2.2rem",
+            display3: "1.6rem",
+            display4: "1.4rem",
+            display5: "1.2rem",
+            display6: "1.1rem",
+            normal: "1rem",
+            small: "0.7rem",
+            smaller: "0.65rem",
+        },
+    },
+};
